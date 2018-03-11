5f00174ba126a83cf3709e4c315a30fa2e8a20e2
Hassan Mostafa <hassanmostafa545@gmail.com>
2018-03-11 15:58:49 +0200

    add audio player

--- AudioVideo-App/ViewController.swift
+++ AudioVideo-App/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player = AVAudioPlayer()
 
+    @IBOutlet weak var slider: UISlider!
+    @IBAction func slideButton(_ sender: Any) {
+        player.volume = slider.value
+    }
+    @IBAction func playButton(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "omar", ofType: "mp3")
+        let url: URL = URL(string: path!)!
+        player = try! AVAudioPlayer(contentsOf: url)
+        player.play()
+    }
+    
+    @IBAction func muteButton(_ sender: Any) {
+        player.volume = 0
+    }
+    @IBAction func stopButton(_ sender: Any) {
+        player.stop()
+    }
+    @IBAction func pauseButton(_ sender: Any) {
+        player.pause()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
